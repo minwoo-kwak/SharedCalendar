dcf7b817ed2269c472f9d7be4801acc3bb038010
Minwoo Kwak <117497001+minwoo-kwak@users.noreply.github.com>
2023-05-25 12:09:31 +0900

    Kiwon (#31)
    * added search function
    
    * added search func
    
    * File System and Alarm CalendarSys Main
    
    * file
    
    ---------
    
    Co-authored-by: lkw <gewonlee99@naver.com>

--- src/FileSystem.java
+++ src/FileSystem.java
@@ -1,25 +1,67 @@
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.List;
 
 public class FileSystem {
-	public void writeFile(String filePath,String fileName,String content) {
+	public boolean writeFile(String filePath,String fileName,StringBuilder sb) {
         try {
             File file = new File(filePath, fileName);
             FileWriter fw = new FileWriter(file,true);
             BufferedWriter bw = new BufferedWriter(fw);
+            String content=sb.toString();
             bw.write(content);
             bw.newLine();
             bw.close();
             fw.close();
-            System.out.println("저장완료.");
-            //return true;
+            //System.out.println("저장완료.");
+            return true;
         } catch (IOException e) {
             System.out.println("파일을 저장하는 도중에 오류가 발생했습니다.");
             e.printStackTrace();
-            //return false;
+            return false;
+            
         }
 	}
+//	public static void save(String filePath, String fileName, Object obj) {
+//      ObjectOutputStream oos = null;
+//        try {
+//			oos = new ObjectOutputStream(new FileOutputStream(filePath+"\\"+fileName));
+//			oos.writeObject(obj);
+//		} catch (Exception e) {
+//			// TODO: handle exception
+//			e.printStackTrace();
+//		}finally {
+//			try {
+//				oos.close();
+//			} catch (Exception e2) {
+//				// TODO: handle exception
+//				e2.printStackTrace();
+//			}
+//		}  
+//	}
+//	public static void load(String filePath,String fileName) {
+//		ObjectInputStream ois = null;
+//		try {
+//			ois = new ObjectInputStream(new FileInputStream(filePath+"\\"+fileName));
+//			//return (List<Object>)ois.readObject();
+//		} catch (Exception e) {
+//			// TODO: handle exception
+//			e.printStackTrace();
+//		}finally {
+//			try {
+//				ois.close();
+//			} catch (Exception e2) {
+//				// TODO: handle exception
+//				e2.printStackTrace();
+//			}
+//		}
+//		return null;
+//	}
 
 }
