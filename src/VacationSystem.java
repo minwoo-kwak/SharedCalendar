a7da92014266520252194e130e91d64199bdc903
KOSA <KOSA@DESKTOP-VKEQ7TB>
2023-05-25 20:44:29 +0900

    병희형 Commit

---
+++ src/VacationSystem.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class VacationSystem {
+	List<Vacation> vacationList = new ArrayList<Vacation>();
+	
+	// 유저들의 연차 정보 저장
+	
+	// 유저들의 연차 정보 불러오고 출력
+	
+	// 유저들의 연차 정보 수정
+}
